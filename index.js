aeb29ab36533aa682c27ca7b2a4828750e076d73
Uederson <uedersonferreira@gmail.com>
2024-03-26 16:42:07 -0300

    Aula1

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+var nome = "Uederson"
+var sobrenome = "Ferreira"
+var anoAtual = 2024
+var idade = 40
+
+var nomeCompl = nome + " " + sobrenome
+var anoNasceu = 1983
+
+console.log("Olá, meu nome é "+nomeCompl+".")
+console.log("Eu nasci em "+anoNasceu+" e tenho "+idade+" anos.")
+console.log("Em "+anoAtual+", estou vivo(a).")
+*/
+
+var n1 = parseInt(prompt("Digite o primeiro número:"))
+var n2 = parseInt(prompt("Agora, digite outro número:"))
+var soma = (n1 + n2)
+var produto = (n1 * n2)
+console.log("A soma de "+n1+" mais "+n2+" é igual a "+soma+".")
+console.log("E a multiplicação entre "+n1+" por "+n2+" é igual a "+produto+".")
+
+alert("A soma de "+n1+" mais "+n2+" é igual a "+soma+". E a multiplicação entre "+n1+" por "+n2+" é igual a "+produto+".")
